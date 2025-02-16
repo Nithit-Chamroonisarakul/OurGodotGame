a0dac7c9c36815c62bd2220e8467d0f611a4909b
Suppanatp7 <suppanat.p@kkumail.com>
2025-02-16 12:57:07 +0700

    Add files via upload

---
+++ Godotgame2-15 (Movement + M1)/InGame/bullet.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@export var speed: float = 400.0
+var direction = Vector2.ZERO
+
+@onready var bullet = $AnimatedSprite2D
+
+func _ready():
+	bullet.play("shootnormal")
+
+func _process(delta):
+	position += direction * speed * delta
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
---
+++ Godotgame2-15 (Movement + M1)/InGame/player.gd
@@ -0,0 +1,48 @@
+extends CharacterBody2D
+
+@export var speed: float = 200.0
+@onready var player = $AnimatedSprite2D  # แก้ให้ตรงกับชื่อจริง
+@onready var bullet_scene = preload("res://InGame/Bullet.tscn")
+
+var last_direction = Vector2.DOWN
+
+func _physics_process(delta):
+	var direction = Vector2.ZERO
+
+	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+
+	if direction.length() > 0:
+		last_direction = direction.normalized()
+
+	if direction.length() > 1:
+		direction = direction.normalized()
+
+	velocity = direction * speed
+	move_and_slide()
+
+	# Animation ตามการเดิน
+	if direction.length() > 0:
+		if abs(direction.x) > abs(direction.y):
+			if direction.x > 0:
+				player.play("walk_right")
+			else:
+				player.play("walk_left")
+		else:
+			if direction.y > 0:
+				player.play("walk_down")
+			else:
+				player.play("walk_up")
+	else:
+		player.play("stand")
+
+func _input(event):
+	if event.is_action_pressed("shoot"):
+		shoot()
+
+func shoot():
+	var bullet = bullet_scene.instantiate()
+	player.play("shootnormal")
+	bullet.position = position
+	bullet.direction = last_direction
+	get_parent().add_child(bullet)
