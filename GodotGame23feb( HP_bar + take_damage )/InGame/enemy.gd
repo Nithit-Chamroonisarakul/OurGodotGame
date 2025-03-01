a22eed97a88001156f1f93b81531da5d4517e8c2
Nithit-Chamroonisarakul <nithit.c@kkumail.com>
2025-03-01 16:16:19 +0700

    HP_Aniamted
    HP_Aniamted

--- GodotGame23feb( HP_bar + take_damage )/InGame/enemy.gd
+++ GodotGame23feb( HP_bar + take_damage )/InGame/enemy.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 @export var speed: float = 100.0 
 @export var stop_distance: float = 20.0  
 
-@export var healthMax: float = 50
+@export var healthMax: float = 100
+@export var knockback_strength: float = 600.0  #boucne
 @onready var currentHealth: float = healthMax
 
 @onready var sprite = $AnimatedSprite2D
@@ -36,9 +37,11 @@ func _physics_process(delta):
 func _on_Hitbox_body_entered(body):
 	if body.is_in_group("player"):
 		body.take_damage(10)
-		queue_free() # ถ้าชน Enermy จะตายทันที
-	
+		#queue_free() # ถ้าชน Enermy จะตายทันที
 func take_damage(damage):
 	currentHealth -= damage
+	var knockback_direction = (global_position - player.global_position).normalized()#boucne
+	velocity = knockback_direction * knockback_strength#boucne
+	move_and_slide()#boucne
 	if currentHealth <= 0:
-		queue_free()  # ลบ Enemy 
+		queue_free()  # ลบ Enemy
